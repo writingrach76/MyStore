43070e35084f8cd6383a2049b076add3904d3534
Riley Baker <vishnukey@gmail.com>
2021-07-04 17:28:36 -0600

    route /item/<id> now returns a single item instead of a singleton list

--- storeserver/database/gen_db.sh
+++ storeserver/database/gen_db.sh
@@ -2,11 +2,18 @@
 
 DB_NAME=storedatabase.db
 SCHEMA_FILE=schema.sqlite
+DATA_FILE=test_data.sqlite
 
+echo "remove old database"
 if [ -e $DB_NAME ] 
 then 
         rm $DB_NAME
 fi
 
 # sqlite3 -batch $DB_NAME $(cat $SCHEMA_FILE)
+echo "creating new database"
 cat $SCHEMA_FILE | sqlite3 -batch $DB_NAME
+echo "database created"
+echo "populating data"
+cat $DATA_FILE | sqlite3 -batch $DB_NAME
+echo "data populated"
